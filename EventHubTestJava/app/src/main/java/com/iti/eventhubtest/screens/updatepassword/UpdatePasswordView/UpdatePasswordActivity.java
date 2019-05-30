42c3476fca9ec0ddc7468bc51afd4abc3b72ac1d
Sahar Hany <sahar96hany@gmail.com>
2019-05-30 18:44:01 +0200

    Initial commit

---
+++ EventHubTestJava/app/src/main/java/com/iti/eventhubtest/screens/updatepassword/UpdatePasswordView/UpdatePasswordActivity.java
@@ -0,0 +1,14 @@
+package com.iti.eventhubtest.screens.updatepassword.UpdatePasswordView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.iti.eventhubtest.R;
+
+public class UpdatePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+    }
+}
